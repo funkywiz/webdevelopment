b2ff86d74d97e6c3945581e5d429c8c163da817a
FunkyWiz <kuttank97@gmail.com>
2017-09-30 22:26:09 +0530

    Backend50%done

---
+++ client/scripts/services/mainservice.js
@@ -0,0 +1,21 @@
+(function(){
+	angular.module('college').factory("MainService",MainService);
+	MainService.$inject=['$http'];
+	function MainService($http){
+		var service={};
+		service.verifydata=verifydata;
+		return service;
+		
+		function verifydata(data){
+			return $http.post('http:localhost:3000/',data).then(successfunction,failurefunction);
+		}
+		function successfunctiion(data){
+			console.log("HANDLED SUCCESSFULLY");
+			return data;
+		}
+		function failurefunction(err){
+			console.log(err);
+			return err;
+		}
+	}
+})();
